834e0e3937b6c54b0a9fc5fbeda8963598ba1510
Evan JIANG <firstfan@gmail.com>
2009-01-24 00:11:30 +0800

    Fix "make modules" script bug under Mac OS X.
    For Mac OS uses BSD sed, it doesn't support "\n". Then, "make modules"
    command cannot work correctly under Mac OS. Using tr command to replace
    sed command, it has the same behavior under both Mac OS and Linux.

--- core/main.mk
+++ core/main.mk
@@ -748,7 +748,7 @@ clobber:
 modules:
 	@echo "Available sub-modules:"
 	@echo "$(call module-names-for-tag-list,$(ALL_MODULE_TAGS))" | \
-	      sed -e 's/  */\n/g' | sort -u | $(COLUMN)
+	      tr ' ' '\n' | grep -v '^$$' | sort -u | $(COLUMN)
 
 .PHONY: showcommands
 showcommands:
