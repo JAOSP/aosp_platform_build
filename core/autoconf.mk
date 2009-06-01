979e07d213f8cda1850993485bbd202a2aa8ed2d
Thinker <thinker@branda.to>
2009-06-01 19:06:06 +0800

    fix dependency issue

--- core/autoconf.mk
+++ core/autoconf.mk
@@ -22,25 +22,38 @@ _saved_LOCAL_PKG_BINARIES:=$(LOCAL_PKG_BINARIES)
 _saved_LOCAL_PKG_SHARED_LIBRARIES:=$(LOCAL_PKG_SHARED_LIBRARIES)
 _saved_LOCAL_PKG_DATA_FILES:=$(LOCAL_PKG_DATA_FILES)
 _saved_LOCAL_CONFIG_ENV:=$(LOCAL_CONFIG_ENV)
+_saved_LOCAL_REQUIRED_MODULES:=$(LOCAL_REQUIRED_MODULES)
 
 # $(1): binary name
 # $(2): class
 define _ac_init_module
 $(eval include $(CLEAR_VARS))
-$(eval _mod_bin:=$(1))
+$(eval _mod_bin:=$(strip $(1)))
 $(eval _mod_name:=$(_my_prefix)$(subst -,_,$(subst .,_,$(_mod_bin))))
 $(eval LOCAL_MODULE:=$(_mod_name))
 $(eval LOCAL_MODULE_CLASS:=$(2))
 $(eval LOCAL_MODULE_STEM:=$(notdir $(_mod_bin)))
 $(eval LOCAL_STRIP_MODULE:=false)
+$(eval LOCAL_REQUIRED_MODULES:=$(_saved_LOCAL_REQUIRED_MODULES))
+
+# Copy libraries to out/target/product/generic/obj/lib
+$(if $(filter SHARED_LIBRARIES,$(2)), \
+	$(eval OVERRIDE_BUILT_MODULE_PATH := \
+	$(TARGET_OUT_INTERMEDIATE_LIBRARIES)))
+
+$(if $(filter $(_ac_first_module),$(_mod_bin)),,\
+	$(eval LOCAL_REQUIRED_MODULES+= \
+		$(_my_prefix)$(subst -,_,$(subst .,_,$(_ac_first_module)))))
 
 $(eval include $(BUILD_SYSTEM)/dynamic_binary.mk)
-$(if $(filter-out $(_ac_first_module),$(_mod_bin)),,\
-	$(eval _ac_work:=$(dir $(linked_module))work))
 
-$(linked_module): $(_ac_work)/make_done
-	$(hide) mkdir -p $(dir $(linked_module));\
-	cp $(_ac_work)/$(_mod_bin) $(linked_module)
+$(if $(filter-out $(_ac_first_module),$(_mod_bin)),,\
+	$(eval _ac_work:=$(dir $(linked_module))work)\
+	$(eval _ac_first_linked:=$(linked_module)))
+
+$(linked_module): $(_ac_work)/mkdone
+	$(hide) if [ ! -e $(dir $(linked_module)) ]; then mkdir -p $(dir $(linked_module)); fi
+	$(hide) cp $(_ac_work)/$(_mod_bin) $(linked_module);
 
 endef
 
@@ -52,10 +65,11 @@ $(eval $(foreach mod,$(_saved_LOCAL_PKG_SHARED_LIBRARIES), \
 
 _ac_mk:= $(_ac_work)/Makefile
 
-$(_ac_work)/make_done: $(_ac_mk)
-	$(hide) make -C $(_ac_work); touch $(_ac_work)
+$(_ac_work)/mkdone: $(_ac_mk)
+	$(hide) cd $(_ac_work); make; touch mkdone
 
-$(_ac_mk): $(_ac_configure)
+$(_ac_mk): $(_ac_configure) $(LOCAL_PATH)/Android.mk
 	$(hide) mkdir -p $(_ac_work); cd $(_ac_work); \
 	$(_saved_LOCAL_CONFIG_ENV) $(TOPDIR)$(_ac_configure) \
 	$(_saved_LOCAL_CONFIG_ARGS)
+	$(hide) touch $(_ac_mk)
