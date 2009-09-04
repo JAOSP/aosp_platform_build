ef302727b663f46a7a039365bfe983a99be1f10d
Alexey Tarasov <tarasov@dodologics.com>
2009-09-04 10:49:37 -0700

    find-jdk-tools-jar.sh now can guess path to tools.jar on FreeBSD

--- core/find-jdk-tools-jar.sh
+++ core/find-jdk-tools-jar.sh
@@ -6,9 +6,24 @@ else
 	echo "Please-install-JDK-5.0,-update-12-or-higher,-which-you-can-download-from-java.sun.com"
 	exit 1
     fi
-    while [ -L $JAVAC ] ; do
-        LSLINE=$(ls -l $JAVAC)
-        JAVAC=$(echo -n $LSLINE | sed -e "s/.* -> //")
-    done
-    echo $JAVAC | sed -e "s:\(.*\)/bin/javac.*:\\1/lib/tools.jar:"
+
+    # XXX: on FreeBSD javac is symlink to javawrapper.sh (javawm),
+    #   which setups environment for one of installed JRE/JDK.
+    #   it's possible to copy most of setJavaHome() function,
+    #   however most probably current JRE is first line in
+    #   /usr/local/etc/javavms
+    if [[ `uname` == FreeBSD ]]; then
+	if [[ ! -e /usr/local/etc/javavms ]] ; then
+	    echo "There is no such file, check JDK installation" > /dev/stderr
+	    exit 1
+	fi
+
+	JAVAC=$(cat /usr/local/etc/javavms | head -n 1)
+    else
+        while [ -L $JAVAC ] ; do
+	    LSLINE=$(ls -l $JAVAC)
+	    JAVAC=$(echo -n $LSLINE | sed -e "s/.* -> //")
+	done
+    fi
+    echo $JAVAC | sed -e "s:\(.*\)/bin/java.*:\\1/lib/tools.jar:"
 fi
