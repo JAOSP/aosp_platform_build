9ae1306bbcc4c80fa819ec0b01633880cea51bf6
Hristo <hristobojinov@yahoo.com>
2009-11-23 12:52:29 -0800

    Commiting modified files as well.

--- tools/retouch/Android.mk
+++ tools/retouch/Android.mk
@@ -53,4 +53,45 @@ LOCAL_MODULE := retouch-prepare
 
 include $(BUILD_HOST_EXECUTABLE)
 
+# Second part: build the encoding test tool, "retouch-deltas".
+# This can be used outside the build process to check the contents
+# of encoded retouch files.
+
+include $(CLEAR_VARS)
+
+LOCAL_LDLIBS += -ldl
+LOCAL_CFLAGS += -O2 -g
+LOCAL_CFLAGS += -fno-function-sections -fno-data-sections -fno-inline
+LOCAL_CFLAGS += -Wall -Wno-unused-function #-Werror
+LOCAL_CFLAGS += -DDEBUG
+
+ifeq ($(TARGET_ARCH),arm)
+LOCAL_CFLAGS += -DARM_SPECIFIC_HACKS
+LOCAL_CFLAGS += -DBIG_ENDIAN=1
+endif
+
+ifeq ($(HOST_OS),darwin)
+LOCAL_CFLAGS += -DFSCANF_IS_BROKEN
+endif
+ifeq ($(HOST_OS),windows)
+LOCAL_CFLAGS += -DFSCANF_IS_BROKEN
+LOCAL_LDLIBS += -lintl
+endif
+
+LOCAL_SRC_FILES := \
+	retouch-deltas.c
+
+LOCAL_C_INCLUDES:= \
+	$(LOCAL_PATH)/
+
+LOCAL_STATIC_LIBRARIES := libelfcopy libelf libebl #dl
+
+ifeq ($(TARGET_ARCH),arm)
+LOCAL_STATIC_LIBRARIES += libebl_arm
+endif
+
+LOCAL_MODULE := retouch-deltas
+
+include $(BUILD_HOST_EXECUTABLE)
+
 endif # TARGET_SIMULATOR != true
--- tools/retouch/retouch-prepare.c
+++ tools/retouch/retouch-prepare.c
@@ -16,6 +16,99 @@
 #include <unistd.h>
 #include <libgen.h>
 
+#define false 0
+#define true 1
+
+int32_t offs_prev;
+uint32_t cont_prev;
+
+void init_compression_state(void) {
+  offs_prev = 0;
+  cont_prev = 0;
+}
+
+//
+// We use three encoding schemes; this takes care of much of the redundancy
+// inherent in lists of relocations:
+//
+//   * two bytes, leading 1, 2b for d_offset ("s"), 13b for d_contents ("c")
+//     
+//     76543210 76543210
+//     1ssccccc cccccccc
+//
+//   * three bytes, leading 01, 2b for delta offset, 20b for delta contents
+//  
+//     76543210 76543210 76543210
+//     01sscccc cccccccc cccccccc
+//
+//   * eigth bytes, leading 00, 30b for offset, 32b for contents
+//
+//     76543210 76543210 76543210 76543210
+//     00ssssss ssssssss ssssssss ssssssss + 4 bytes contents
+//
+// NOTE 1: All deltas are w.r.t. the previous line in the list.
+// NOTE 2: Two-bit ("ss") offsets mean: "00"=4, "01"=8, "10"=12, and "11"=16.
+// NOTE 3: Delta contents are signed. To map back to a int32 we refill with 1s.
+// NOTE 4: Special encoding for -1 offset. Extended back to 32b when decoded.
+//
+
+bool encode(FILE *f_out, int32_t offset, uint32_t contents) {
+  int64_t d_offs = offset-offs_prev;
+  int64_t d_cont = (int64_t)contents-(int64_t)cont_prev;
+
+  uint8_t output[8];
+  uint32_t output_size;
+  
+  if ((d_offs > 3) &&
+      (d_offs % 4) == 0 &&
+      (d_offs / 4) < 5 &&
+      (d_cont < 4000) &&
+      (d_cont > -4000)) {
+    // we can fit in 2 bytes
+    output[0] = 
+      0x80 | 
+      (((d_offs/4)-1) << 5) | 
+      (((uint64_t)d_cont & 0x1f00) >> 8);
+    output[1] = 
+      ((uint64_t)d_cont & 0xff);
+    output_size = 2;
+  } else if ((d_offs > 3) &&
+	     (d_offs % 4) == 0 &&
+	     (d_offs / 4) < 5 &&
+	     (d_cont < 510000) &&
+	     (d_cont > -510000)) {
+    // fit in 3 bytes
+    output[0] = 
+      0x40 | 
+      (((d_offs/4)-1) << 4) | 
+      (((uint64_t)d_cont & 0xf0000) >> 16);
+    output[1] = 
+      ((uint64_t)d_cont & 0xff00) >> 8;
+    output[2] = 
+      ((uint64_t)d_cont & 0xff);
+    output_size = 3;
+  } else {
+    // fit in 8 bytes; we can't support files bigger than (1GB-1) 
+    // with this encoding: no library is that big anyway..
+    if (offset < -1 || offset > 0x3ffffffe) return false;
+    output[0] = (offset & 0x3f000000) >> 24;
+    output[1] = (offset & 0xff0000) >> 16;
+    output[2] = (offset & 0xff00) >> 8;
+    output[3] = (offset & 0xff);
+    output[4] = (contents & 0xff000000) >> 24;
+    output[5] = (contents & 0xff0000) >> 16;
+    output[6] = (contents & 0xff00) >> 8;
+    output[7] = (contents & 0xff);
+    output_size = 8;
+  }
+  
+  if (fwrite(&output, 1, output_size, f_out) != output_size) return false;
+  
+  offs_prev = offset;
+  cont_prev = contents;
+  return true;
+}
+
 #define MAX_FULL_NAME 1024
 
 #define FAILIF(cond, msg...) do { \
@@ -29,7 +122,7 @@ int is_host_little(void) {
   short val = 0x10; return ((char *)&val)[0] != 0;
 }
 
-int32_t switch_endianness(int32_t val)
+uint32_t switch_endianness(uint32_t val)
 {
   int32_t newval;
   ((char *)&newval)[3] = ((char *)&val)[0];
@@ -99,10 +192,12 @@ void output_prelink_info(FILE *f_out,
 
   if (!(elf_little ^ is_host_little())) {
     /* Same endianness */
-    fprintf(f_out, "-1 %lu\n", info.mmap_addr);
+    FAILIF(!encode(f_out, -1, info.mmap_addr),
+	   "Could not encode prelink info!\n");
   } else {
     /* Different endianness */
-    fprintf(f_out, "-1 %lu\n", switch_endianness(info.mmap_addr));
+    FAILIF(!encode(f_out, -1, switch_endianness(info.mmap_addr)),
+	   "Could not encode prelink info!\n");
   }
 }
 
@@ -115,10 +210,11 @@ void output_relocation(FILE *f_out,
 	 "Could not seek for reading!\n");
   FAILIF(read(fd_lib, &retouch_contents, 4) != 4,
          "Could not read retouch bytes!\n");
-  fprintf(f_out, "%ld %lu\n", offset, 
-	  (!(elf_little ^ is_host_little()))?
-	  retouch_contents:
-	  switch_endianness(retouch_contents));
+  FAILIF(!encode(f_out, offset, 
+		 (!(elf_little ^ is_host_little()))?
+		 retouch_contents:
+		 switch_endianness(retouch_contents)),
+	 "Could not encode relocation!\n");
 }
 
 int main(int argc, char **argv) {
@@ -180,13 +276,14 @@ int main(int argc, char **argv) {
   FAILIF((file_apriori = fopen(argv[1], "r")) == NULL,  
 	 "Could not fopen(\"%s\")\n", 
 	 argv[1]);
-  FAILIF((file_retouch = fopen(argv[3], "w")) == NULL,  
+  FAILIF((file_retouch = fopen(argv[3], "wb")) == NULL,  
 	 "Could not fopen(\"%s\")\n", 
 	 argv[3]);
 
   // loop over all touch-up entries
   line_count = 0;
   current_lib_full_name[0] = 0;
+  init_compression_state();
   while (!feof(file_apriori)) {
     char one_line[MAX_FULL_NAME];
 
@@ -203,7 +300,7 @@ int main(int argc, char **argv) {
     }
     line_count++;
 
-    // find the section and fix at the specified offset
+    // find the section and map the section+offset to a file offset
     uint32_t sectIx;
     for (sectIx=0; sectIx<shnum; sectIx++) {
       Elf_Scn *scn;
