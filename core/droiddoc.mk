fc047f4ab2b2414b6d7389bf185f7440fec1d81a
Sebastian Rasmussen <sebastian.rasmussen@stericsson.com>
2011-02-13 14:23:16 +0100

    Remove droiddoc error print-outs
    During a build, droiddoc error  print-outs such as below are
    produced.
    
    find: 'frameworks/base/docs/html': No such file or directory
    
    This fix will remove these print-outs which creates a cleaner
    build log.
    
    Change-Id: I8ec0fe145818a07c015fa419a8b6a931b77d9345
    Signed-off-by: Christian Bejram <christian.bejram@stericsson.com>

--- core/droiddoc.mk
+++ core/droiddoc.mk
@@ -144,7 +144,7 @@ endif
 # TODO: not clear if this is used any more
 $(full_target): PRIVATE_LOCAL_PATH := $(LOCAL_PATH)
 
-html_dir_files := $(shell find $(LOCAL_PATH)/$(LOCAL_DROIDDOC_HTML_DIR) -type f)
+html_dir_files := $(shell if [ -d "$(LOCAL_PATH)/$(LOCAL_DROIDDOC_HTML_DIR)" ] ; then find $(LOCAL_PATH)/$(LOCAL_DROIDDOC_HTML_DIR) -type f ; fi)
 
 $(full_target): $(full_src_files) $(droiddoc_templates) $(droiddoc) $(html_dir_files) $(full_java_lib_deps) $(LOCAL_ADDITIONAL_DEPENDENCIES)
 	@echo Docs droiddoc: $(PRIVATE_OUT_DIR)
