37061993763304a4422c6045d14857186f184b62
Bruce Beare <bruce.j.beare@intel.com>
2011-05-31 13:00:50 -0700

    x86: Add in the VirtualBox emulator as a separate build target
    Orig-Change-Id: Icff81a757952b7652d171bc95773e177c4fd7772
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>

--- target/product/AndroidProducts.mk
+++ target/product/AndroidProducts.mk
@@ -36,6 +36,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/core.mk \
     $(LOCAL_DIR)/generic.mk \
     $(LOCAL_DIR)/generic_x86.mk \
+    $(LOCAL_DIR)/vbox_x86.mk \
     $(LOCAL_DIR)/full.mk \
     $(LOCAL_DIR)/sdk.mk \
     $(LOCAL_DIR)/sim.mk
