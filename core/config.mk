dadaf4f3eded4d5a81094f30503313ca9544c070
Stephen Smalley <sds@tycho.nsa.gov>
2012-01-13 08:05:58 -0500

    Add libselinux/include to SRC_HEADERS.
    This was required in order to pick up the libselinux headers for
    building bootable/recovery, dalvik, external/yaffs2, frameworks/base,
    system/core, and system/extras.
    
    Change-Id: Ib04d37b94afe661426bbbc6298aca4d5a6a85497

--- core/config.mk
+++ core/config.mk
@@ -25,6 +25,7 @@ SRC_DOCS:= $(TOPDIR)docs
 # TODO: See if we can remove most of these from the global list.
 SRC_HEADERS := \
 	$(TOPDIR)system/core/include \
+	$(TOPDIR)external/libselinux/include \
 	$(TOPDIR)hardware/libhardware/include \
 	$(TOPDIR)hardware/libhardware_legacy/include \
 	$(TOPDIR)hardware/ril/include \
