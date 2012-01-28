42a10acf4df783dbab438655cf1ee0b8690f6127
John Hsing <tsyj2007@gmail.com>
2012-01-28 13:53:05 +0800

    Set JVM -Xmx to 512M
    In fact, too large JVM memory is not needed.
    
    With 512M memory, my machine with 2G memory and 32 bit OS runs well.
    
    Change-Id: Ib45e9d30af10315adf0f1a4216e09be069260086

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -355,7 +355,7 @@ def SignFile(input_name, output_name, key, password, align=None,
   else:
     sign_name = output_name
 
-  cmd = ["java", "-Xmx2048m", "-jar",
+  cmd = ["java", "-Xmx512m", "-jar",
            os.path.join(OPTIONS.search_path, "framework", "signapk.jar")]
   if whole_file:
     cmd.append("-w")
