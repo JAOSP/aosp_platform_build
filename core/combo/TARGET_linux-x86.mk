0576514621affefea86a14d419f428761a60e067
Bruce Beare <bruce.j.beare@intel.com>
2012-02-15 16:39:20 -0800

    x86: Clean up atom vs/ non-atom compiler flags, enable SSSE3

--- core/combo/TARGET_linux-x86.mk
+++ core/combo/TARGET_linux-x86.mk
@@ -96,8 +96,8 @@ TARGET_GLOBAL_CPPFLAGS += \
 			-fno-use-cxa-atexit
 
 ifeq ($(TARGET_ARCH_VARIANT),x86-atom)
-    # Basic ATOM flags.
-    TARGET_GLOBAL_CFLAGS += -march=atom -mstackrealign -mfpmath=sse
+    # Basic ATOM flags - only use this if you have both ssse3 and movbe instructions
+    TARGET_GLOBAL_CFLAGS += -march=atom -mstackrealign -mfpmath=sse -mno-movbe -m32
 
     # There are various levels of ATOM processors out there. Different ones have different
     # capabilities. This first define matches the NDK's minimum ABI requirements.
@@ -108,14 +108,15 @@ ifeq ($(TARGET_ARCH_VARIANT),x86-atom)
     # If you wish to build a BSP that will only be used on hardware that has additional
     # available instructions, enable them here. By default, this is commented off so that
     # the default images can run on all processors that are NDK ABI compliant.
-    # TARGET_GLOBAL_CFLAGS += -DUSE_SSSE3
+    TARGET_GLOBAL_CFLAGS += -DUSE_SSSE3
 else
-    # Plain 'x86' - lowest common denominator. This should run pretty much on any hardware.
+    # Plain 'x86' - Requires sse3, use if you have nossse3 or movbe
     #
     # Note: The NDK's ABI (see the NDK ABI documentation) requires many of the more recent
     # instruction set additions. You can build an "x86" BSP that will run on very old hardware,
     # but it won't be able to run much of the x86 NDK compliant code.
-    TARGET_GLOBAL_CFLAGS += -march=i686
+    TARGET_GLOBAL_CFLAGS += -march=i686 -msse3 -mfpmath=sse -m32
+    TARGET_GLOBAL_CFLAGS += -DUSE_MMX -DUSE_SSE -DUSE_SSE2 -DUSE_SSE3
 endif
 
 TARGET_GLOBAL_CFLAGS += -mbionic
