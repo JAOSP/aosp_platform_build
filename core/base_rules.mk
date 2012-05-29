61c5ca410b19506514b8c964537bf84cb8cd811d
Pekka Nikander <pekka.nikander@senseg.com>
2012-05-29 12:01:41 +0300

    Support using LOCAL_STATIC_JAVA_LIBRARIES for Android Java libraries
    LOCAL_STATIC_JAVA_LIBRARIES now expands conditionally either to point to the
    javalib.jar (as before) or to classes.jar (only for Android Java libraries).
    
    While host javalib.jar files contain the compiled Java .class files,
    Android javalib.jar files contain only the classes.dex.  To provide
    javac with .class files when compiling for Anrdoird, classes.jar is
    needed instead.
    
    Signed-off-by: Pekka Nikander <nikander.pekka@gmail.com>

--- core/base_rules.mk
+++ core/base_rules.mk
@@ -383,10 +383,17 @@ endif # java_resource_file_groups
 
 ifneq ($(strip $(all_java_sources)$(all_res_assets)),)
 
+ifdef LOCAL_IS_HOST_MODULE
 full_static_java_libs := \
     $(foreach lib,$(LOCAL_STATIC_JAVA_LIBRARIES), \
       $(call intermediates-dir-for, \
         JAVA_LIBRARIES,$(lib),$(LOCAL_IS_HOST_MODULE),COMMON)/javalib.jar)
+else
+full_static_java_libs := \
+    $(foreach lib,$(LOCAL_STATIC_JAVA_LIBRARIES), \
+      $(call intermediates-dir-for, \
+        JAVA_LIBRARIES,$(lib),$(LOCAL_IS_HOST_MODULE),COMMON)/classes.jar)
+endif # LOCAL_IS_HOST_MODULE
 
 $(LOCAL_INTERMEDIATE_TARGETS): PRIVATE_INSTALL_DIR := $(dir $(LOCAL_INSTALLED_MODULE))
 $(LOCAL_INTERMEDIATE_TARGETS): PRIVATE_CLASS_INTERMEDIATES_DIR := $(intermediates)/classes
