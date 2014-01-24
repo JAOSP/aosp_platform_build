d46bc1f2f7491c2a7c5f325e047c5f4b996ca0e1
Colin Cross <ccross@android.com>
2014-01-24 13:38:08 -0800

    HACK: add 64-bit directory blacklist
    Change-Id: I431e8b220db81d51930dc0b3d95995df120c179a

---
+++ core/64_bit_blacklist.mk
@@ -0,0 +1,64 @@
+ifneq ($(TARGET_2ND_ARCH),)
+
+# VMs - need 64-bit support
+_64_bit_directory_blacklist := \
+	art \
+	dalvik \
+
+# JNI - needs 64-bit VM
+_64_bit_directory_blacklist += \
+	external/conscrypt \
+	external/neven \
+	external/svox \
+	libcore \
+	packages \
+
+# Chromium/V8: needs 64-bit support
+_64_bit_directory_blacklist += \
+	external/chromium \
+	external/chromium-libpac \
+	external/chromium_org \
+	external/skia \
+	external/v8 \
+	frameworks/webview \
+
+# misc build errors
+_64_bit_directory_blacklist += \
+	external/bluetooth/bluedroid \
+	external/compiler-rt \
+	external/llvm \
+	external/oprofile/opcontrol \
+	external/speex \
+	external/srec \
+	external/tcpdump \
+	frameworks/av \
+	frameworks/base \
+	frameworks/compile \
+	frameworks/ex \
+	frameworks/ml \
+	frameworks/opt \
+	frameworks/rs \
+	frameworks/wilhelm \
+	system/media/audio_utils \
+	device/generic/goldfish/opengl \
+	device/generic/goldfish/camera \
+
+# depends on libnl_2
+_64_bit_directory_blacklist += \
+	external/android-clat \
+	external/wpa_suplicant_8
+
+# depends on frameworks/av
+_64_bit_directory_blacklist += \
+	hardware/libhardware_legacy/audio \
+	hardware/libhardware/modules/audio_remote_submix \
+
+_64_bit_directory_blacklist_pattern := $(addsuffix %,$(_64_bit_directory_blacklist))
+
+define directory_is_64_bit_blacklisted
+$(if $(filter $(_64_bit_directory_blacklist_pattern),$(1)),true)
+endef
+else
+define directory_is_64_bit_blacklisted
+endef
+endif
