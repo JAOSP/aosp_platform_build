cc4d2cb3f2f0329cb24351c28e254f7c3cc7ff9d
Colin Cross <ccross@android.com>
2014-01-29 18:47:08 -0800

    add support for BUILD_PREFER_32_BIT
    Allow a BoardConfig.mk file to specify that by default executables
    should be built for 32-bits, for example for a svelte device.
    Modules that specify LOCAL_NO_2ND_ARCH will still be built for
    64-bit in a 64-bit build.
    
    Change-Id: Ia291eb88bab2af746cf7c581eb547ad341a5a4cc

--- core/executable.mk
+++ core/executable.mk
@@ -3,10 +3,13 @@
 # By default, an executable is built for TARGET_ARCH.
 # To build it for TARGET_2ND_ARCH in a 64bit product, use "LOCAL_32_BIT_ONLY := true".
 
-ifeq ($(TARGET_IS_64_BIT)|$(LOCAL_32_BIT_ONLY),true|true)
-LOCAL_2ND_ARCH_VAR_PREFIX := $(TARGET_2ND_ARCH_VAR_PREFIX)
-else
 LOCAL_2ND_ARCH_VAR_PREFIX :=
+ifeq ($(TARGET_IS_64_BIT),true)
+ifeq ($(LOCAL_32_BIT_ONLY),true)
+LOCAL_2ND_ARCH_VAR_PREFIX := $(TARGET_2ND_ARCH_VAR_PREFIX)
+else ifeq ($(BUILD_PREFER_32_BIT)|$(LOCAL_NO_2ND_ARCH),true|)
+LOCAL_2ND_ARCH_VAR_PREFIX := $(TARGET_2ND_ARCH_VAR_PREFIX)
+endif
 endif
 
 include $(BUILD_SYSTEM)/executable_internal.mk
