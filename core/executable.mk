83317d97b1005ddddb56933e9d4cc5d981444de4
Colin Cross <ccross@android.com>
2014-02-06 14:45:37 -0800

    add support for module supported or unsupported target architectures
    Add two new variables for module makefiles:
    
    LOCAL_MODULE_TARGET_ARCH specifies that a module is only supported for
    one or more architectures.  Any architecture not in the list will be
    not attempt to build the module.  The expected use case is prebuilts
    that are only suitable for a single architecture, or modules like llvm
    that need per-architecture support.
    
    LOCAL_MODULE_UNSUPPORTED_TARGET_ARCH specifies that a module cannot be
    built for one or more architectures.
    
    The logic for whether or not to build an architecture is fairly
    complicated, so this patch consolidates it into module_arch_supported.mk
    
    Change-Id: I120caf4a375f484e1fd6017b60c2f53882ae01e6

--- core/executable.mk
+++ core/executable.mk
@@ -3,13 +3,25 @@
 # By default, an executable is built for TARGET_ARCH.
 # To build it for TARGET_2ND_ARCH in a 64bit product, use "LOCAL_32_BIT_ONLY := true".
 
-ifeq ($(TARGET_IS_64_BIT)|$(LOCAL_32_BIT_ONLY),true|true)
-LOCAL_2ND_ARCH_VAR_PREFIX := $(TARGET_2ND_ARCH_VAR_PREFIX)
-else
-LOCAL_2ND_ARCH_VAR_PREFIX :=
-endif
-
 LOCAL_NO_2ND_ARCH_MODULE_SUFFIX := true
+
+# check if primary arch is supported
+LOCAL_2ND_ARCH_VAR_PREFIX :=
+include $(BUILD_SYSTEM)/module_arch_supported.mk
+ifeq ($(my_module_arch_supported),true)
+# primary arch is supported
 include $(BUILD_SYSTEM)/executable_internal.mk
+else ifneq (,$(TARGET_2ND_ARCH))
+# check if secondary arch is supported
+LOCAL_2ND_ARCH_VAR_PREFIX := $(TARGET_2ND_ARCH_VAR_PREFIX)
+include $(BUILD_SYSTEM)/module_arch_supported.mk
+ifeq ($(my_module_arch_supported),true)
+# secondary arch is supported
+include $(BUILD_SYSTEM)/executable_internal.mk
+endif
+endif # TARGET_2ND_ARCH
+
 LOCAL_2ND_ARCH_VAR_PREFIX :=
 LOCAL_NO_2ND_ARCH_MODULE_SUFFIX :=
+
+my_module_arch_supported :=
