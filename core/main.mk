0dd0cfe99fd8503feeac6fd87272e2051945e931
Brian Carlstrom <bdc@google.com>
2014-02-10 17:52:53 -0800

    Add dalvik.vm.dex2oat-flags for eng build
    Change-Id: I794320267c846225931f12075c7f32d7848081e9

--- core/main.mk
+++ core/main.mk
@@ -383,6 +383,10 @@ ifneq ($(filter ro.setupwizard.mode=ENABLED, $(call collapse-pairs, $(ADDITIONAL
           $(call collapse-pairs, $(ADDITIONAL_BUILD_PROPERTIES))) \
           ro.setupwizard.mode=OPTIONAL
 endif
+# Don't compile the image on eng builds to speed startup
+ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.image-dex2oat-flags=--compiler-filter=interpret-only
+# Don't compile apps on eng builds to speed startup
+ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.dex2oat-flags=--compiler-filter=interpret-only
 endif
 
 ## sdk ##
