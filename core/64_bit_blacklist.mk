fe3a0e713ac675f5b2c0663e5a6e149cd1bd26b9
Narayan Kamath <narayan@google.com>
2014-02-28 15:34:16 +0000

    Amend 64 bit blacklist.
    - external/skia now builds on arm64, frameworks/base/libs/hwui
      depends on it.
    - external/bluetooth/bluedroid builds on 64 bit, though there
      isn't any obvious way to test it without real hardware.
    - frameworks/ml now builds, as does external/srec
    - frameworks/opt, frameworks/ex and frameworks/wilhelm don't
      build because of their dependency on frameworks/av.
    
    frameworks/av should probably be dropped out of the blacklist
    and replaced by individual markers on targets that we want
    for 64 bit (we don't want all of them for arm64).
    
    Change-Id: I9735108840fcba21ac8918bedf0d6de8989de086

--- core/64_bit_blacklist.mk
+++ core/64_bit_blacklist.mk
@@ -12,30 +12,26 @@ _64_bit_directory_blacklist += \
 	external/chromium \
 	external/chromium-libpac \
 	external/chromium_org \
-	external/skia \
 	external/v8 \
 	frameworks/webview \
 
 # misc build errors
 _64_bit_directory_blacklist += \
-	external/bluetooth/bluedroid \
 	external/oprofile/daemon \
 	external/oprofile/opcontrol \
 	external/tcpdump \
 	frameworks/av \
 	frameworks/base \
-	frameworks/ex \
-	frameworks/ml \
-	frameworks/opt \
-	frameworks/wilhelm \
 	device/generic/goldfish/opengl \
 	device/generic/goldfish/camera \
 
 # depends on frameworks/av
 _64_bit_directory_blacklist += \
-	external/srec \
 	hardware/libhardware_legacy/audio \
 	hardware/libhardware/modules/audio_remote_submix \
+	frameworks/wilhelm \
+	frameworks/opt \
+	frameworks/ex \
 
 _64_bit_directory_blacklist_pattern := $(addsuffix %,$(_64_bit_directory_blacklist))
 
