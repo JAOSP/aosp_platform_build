9c0bdfdd321a5273b4337c416033732beba21168
Guilhem IMBERTON <guilhem.imberton@intel.com>
2014-03-11 11:15:04 +0100

    Fix modules dependency in prebuilds
    Adding LOCAL_REQUIRED_MODULES to *_PREBUILT.
    This modification fix prebuilt dependency
    
    Change-Id: Icd01c5b44ff7a427ae5e8e1cf19f265113a16f5e
    Signed-off-by: Guilhem IMBERTON <guilhem.imberton@intel.com>

--- core/multi_prebuilt.mk
+++ core/multi_prebuilt.mk
@@ -28,6 +28,7 @@ prebuilt_static_java_libraries := $(LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES)
 prebuilt_is_host := $(LOCAL_IS_HOST_MODULE)
 prebuilt_module_tags := $(LOCAL_MODULE_TAGS)
 prebuilt_strip_module := $(LOCAL_STRIP_MODULE)
+prebuilt_required_modules := $(LOCAL_REQUIRED_MODULES)
 
 
 ifndef multi_prebuilt_once
@@ -37,10 +38,12 @@ multi_prebuilt_once := true
 # $(2): IS_HOST_MODULE
 # $(3): MODULE_CLASS
 # $(4): MODULE_TAGS
-# $(5): OVERRIDE_BUILT_MODULE_PATH
-# $(6): UNINSTALLABLE_MODULE
-# $(7): BUILT_MODULE_STEM
-# $(8): LOCAL_STRIP_MODULE
+# $(5): LOCAL_REQUIRED_MODULES
+# $(6): OVERRIDE_BUILT_MODULE_PATH
+# $(7): UNINSTALLABLE_MODULE
+# $(8): BUILT_MODULE_STEM
+# $(9): LOCAL_STRIP_MODULE
+
 #
 # Elements in the file list may be bare filenames,
 # or of the form "<modulename>:<filename>".
@@ -55,8 +58,9 @@ $(foreach t,$(1), \
   $(eval LOCAL_IS_HOST_MODULE := $(2)) \
   $(eval LOCAL_MODULE_CLASS := $(3)) \
   $(eval LOCAL_MODULE_TAGS := $(4)) \
-  $(eval OVERRIDE_BUILT_MODULE_PATH := $(5)) \
-  $(eval LOCAL_UNINSTALLABLE_MODULE := $(6)) \
+  $(eval LOCAL_REQUIRED_MODULES := $(5)) \
+  $(eval OVERRIDE_BUILT_MODULE_PATH := $(6)) \
+  $(eval LOCAL_UNINSTALLABLE_MODULE := $(7)) \
   $(eval tw := $(subst :, ,$(strip $(t)))) \
   $(if $(word 3,$(tw)),$(error $(LOCAL_PATH): Bad prebuilt filename '$(t)')) \
   $(if $(word 2,$(tw)), \
@@ -66,14 +70,14 @@ $(foreach t,$(1), \
     $(eval LOCAL_MODULE := $(basename $(notdir $(t)))) \
     $(eval LOCAL_SRC_FILES := $(t)) \
    ) \
-  $(if $(7), \
-    $(eval LOCAL_BUILT_MODULE_STEM := $(7)) \
+  $(if $(8), \
+    $(eval LOCAL_BUILT_MODULE_STEM := $(8)) \
    , \
     $(eval LOCAL_BUILT_MODULE_STEM := $(notdir $(LOCAL_SRC_FILES))) \
    ) \
   $(eval LOCAL_MODULE_SUFFIX := $(suffix $(LOCAL_SRC_FILES))) \
   $(if $(filter user,$(TARGET_BUILD_VARIANT)), \
-    $(eval LOCAL_STRIP_MODULE := $(8))) \
+    $(eval LOCAL_STRIP_MODULE := $(9))) \
   $(eval include $(BUILD_PREBUILT)) \
  )
 endef
@@ -86,6 +90,7 @@ $(call auto-prebuilt-boilerplate, \
     $(prebuilt_is_host), \
     STATIC_LIBRARIES, \
     $(prebuilt_module_tags), \
+    $(prebuilt_required_modules), \
     , \
     true)
 
@@ -94,6 +99,7 @@ $(call auto-prebuilt-boilerplate, \
     $(prebuilt_is_host), \
     SHARED_LIBRARIES, \
     $(prebuilt_module_tags), \
+    $(prebuilt_required_modules), \
     $($(if $(prebuilt_is_host),HOST,TARGET)_OUT_INTERMEDIATE_LIBRARIES), \
     , \
     , \
@@ -103,13 +109,15 @@ $(call auto-prebuilt-boilerplate, \
     $(prebuilt_executables), \
     $(prebuilt_is_host), \
     EXECUTABLES, \
-    $(prebuilt_module_tags))
+    $(prebuilt_module_tags), \
+    $(prebuilt_required_modules))
 
 $(call auto-prebuilt-boilerplate, \
     $(prebuilt_java_libraries), \
     $(prebuilt_is_host), \
     JAVA_LIBRARIES, \
     $(prebuilt_module_tags), \
+    $(prebuilt_required_modules), \
     , \
     , \
     javalib.jar)
@@ -119,6 +127,7 @@ $(call auto-prebuilt-boilerplate, \
     $(prebuilt_is_host), \
     JAVA_LIBRARIES, \
     $(prebuilt_module_tags), \
+    $(prebuilt_required_modules), \
     , \
     true, \
     javalib.jar)
@@ -130,3 +139,4 @@ prebuilt_java_libraries :=
 prebuilt_static_java_libraries :=
 prebuilt_is_host :=
 prebuilt_module_tags :=
+prebuilt_required_modules :=
