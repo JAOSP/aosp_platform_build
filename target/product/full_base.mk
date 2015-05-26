06af27693a52b12fff3977aa73858e867c2fa9d2
Akihiro Maeda <sola.1980.a@gmail.com>
2015-05-26 17:52:11 +0900

    Delete the sound settings of the default

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -39,10 +39,10 @@ PRODUCT_PACKAGES += \
     PhotoTable
 
 # Additional settings used in all AOSP builds
-PRODUCT_PROPERTY_OVERRIDES := \
-    ro.com.android.dateformat=MM-dd-yyyy \
-    ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg
+#PRODUCT_PROPERTY_OVERRIDES := \
+#    ro.com.android.dateformat=MM-dd-yyyy \
+#    ro.config.ringtone=Ring_Synth_04.ogg \
+#    ro.config.notification_sound=pixiedust.ogg
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
@@ -51,7 +51,7 @@ PRODUCT_LOCALES := en_US
 PRODUCT_AAPT_CONFIG := normal
 
 # Get some sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+#$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
 
 # Get the TTS language packs
 $(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
