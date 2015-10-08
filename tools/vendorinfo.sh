963d84d1ff441affadf1bd2e0e87e69841a26427
Akihiro Maeda <sola.1980.a@gmail.com>
2015-10-09 01:15:10 +0900

    Fix vendorinfo.sh

--- tools/vendorinfo.sh
+++ tools/vendorinfo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "ro.build.date=`LANG=en date`"
-echo "ro.build.date.utc=`LANG=en date +%s`"
+echo "ro.vendor.build.date=`LANG=en date`"
+echo "ro.vendor.build.date.utc=`LANG=en date +%s`"
 echo "ro.vendor.build.fingerprint=$BUILD_FINGERPRINT"
