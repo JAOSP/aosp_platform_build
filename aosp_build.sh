e254834da9bafa7041e1be88d07daa9821972c50
Akihiro Maeda <sola.1980.a@gmail.com>
2015-10-09 10:10:05 +0900

    Update aosp_build.sh for Android 6.0

--- aosp_build.sh
+++ aosp_build.sh
@@ -13,33 +13,21 @@ PARALLEL_NUM=$4
 source ~/bin/android-env.sh
 source build/envsetup.sh
 export USE_CCACHE=1
-./prebuilts/misc/linux-x86/ccache/ccache -M 100G
 
 lunch ${BUILD_INFO}_${TARGET_NAME}-${BUILD_TYPE}
 
-if [ ${TARGET_NAME} = "mako" -o ${TARGET_NAME} = "hammerhead" ]; then
+if [ ${TARGET_NAME} = "hammerhead" ]; then
     VENDOR_NAME="lge"
-elif [ ${TARGET_NAME} = "grouper" -o ${TARGET_NAME} = "flo" ]; then
+elif [ ${TARGET_NAME} = "flo" ]; then
     VENDOR_NAME="asus"
-elif [ ${TARGET_NAME} = "manta" ]; then
-    VENDOR_NAME="samsung"
+elif [ ${TARGET_NAME} = "shamu" ]; then
+    VENDOR_NAME="moto"
 elif [ ${TARGET_NAME} = "flounder" ]; then
     VENDOR_NAME="htc"
 fi
 
-if [ ${TARGET_NAME} = "mako" -o ${TARGET_NAME} = "grouper" -o ${TARGET_NAME} = "flo" -o ${TARGET_NAME} = "manta" ]; then
-cd device/${VENDOR_NAME}/${TARGET_NAME}/
-./download-blobs.sh
-cd -
-cd vendor/aosp/${VENDOR_NAME}/${TARGET_NAME}/proprietary/
-rm -rf system
-rm -rf *.tgz
-./extract-files.sh
-cd -
-elif [ ${TARGET_NAME} = "hammerhead" -o ${TARGET_NAME} = "flounder" ]; then
 cd device/${VENDOR_NAME}/${TARGET_NAME}/
 ./extract-files.sh
 cd -
-fi
 
 make otapackage -j${PARALLEL_NUM}
